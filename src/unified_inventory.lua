91ab0c637c7c0feb455769c559e039d018a1d7a9
EISHU <109120433+EISHU92@users.noreply.github.com>
2024-02-11 22:26:55 -0300

    volar shop de inventario

--- src/unified_inventory.lua
+++ src/unified_inventory.lua
@@ -12,16 +12,5 @@ if minetest.get_modpath("unified_inventory") then
 		end,
 	})
 
-	unified_inventory.register_button("xp_shop", {
-		type = "image",
-		image = "xp_shop_button.png",
-		tooltip = "XP Shop",
-		hide_lite=true,
-		action = function(player)
-			minetest.show_formspec(player:get_player_name(), "xp:shop", show_shop_formspec(player))
-		end,
-		condition = function(player)
-			return minetest.check_player_privs(player:get_player_name(), {interact=true})
-		end,
-	})
+	
 end
